7199ffa84b16a144da5ae1657cd0f284121795b1
Dmitry Kokorin <dmitry@kokorin.org>
2013-09-28 17:56:16 +0400

    fix

---
+++ scatmc.cpp
@@ -0,0 +1,535 @@
+#include "scatmc.h"
+
+#include "mathcompat.h"
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <omp.h>
+
+
+#include "common.h"
+
+#include "channel.h"
+#include "partition.h"
+#include "photon.h"
+#include "indicatrix.h"
+#include "freepath.h"
+#include "optics.h"
+#include "distance.h"
+
+
+
+
+int main(int argc, char ** argv)
+{
+    ScatMCApp app;
+
+    int res = 0;
+
+    if (app.getOpts(argc, argv)) {
+
+        res = app.run();
+    }
+    else {
+
+        app.printHelp();
+        res = -1;
+    }
+
+    return res;
+}
+
+
+/////////////////////////////////////////////
+
+
+ScatMCApp::ScatMCApp()
+  : options_()
+  , m_eLength()
+  , m_oLength()
+  , m_eChannelProb()
+  , m_eEscFunction()
+  , m_oEscFunction()
+  , m_photonCnt(0)
+  , m_saveRate(0)
+  , m_dataBuff()
+{
+}
+
+bool ScatMCApp::getOpts(int argc, char ** argv)
+{
+    options_.execName = argv[0];
+
+    for (int i = 1; i < argc; ++i) {
+
+        std::string arg = argv[i];
+
+        if (arg == "--workdir") {
+
+            if (++i == argc)
+                return false;
+
+            options_.workDir = argv[i];
+
+            if (!options_.workDir.empty())
+                options_.workDir += '/';
+        }
+        else if (arg == "--seed") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.seed;
+        }
+        else if (arg == "--photons") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.maxPhotons;
+        }
+        else if (arg == "--phisize") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.phiSize;
+        }
+        else if (arg == "--thetasize") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.thetaSize;
+        }
+        else if (arg == "--maxtheta") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.maxTheta;
+        }
+        else if (arg == "--escphisize") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.escPhiSize;
+        }
+        else if (arg == "--escthetasize") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.escThetaSize;
+        }
+        else if (arg == "--esczsize") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.escZSize;
+        }
+        else if (arg == "--escmaxz") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.escMaxZ;
+        }
+        else if (arg == "--scatterings") {
+
+            if (++i == argc)
+                return false;
+
+            std::stringstream stream(argv[i]);
+            stream >> options_.maxScatterings;
+        }
+        else if (arg == "--loadoeprofile") {
+
+            if (++i == argc)
+                return false;
+
+            options_.oeProfileOptions = Load;
+            options_.oeProfileName    = argv[i];
+        }
+        else if (arg == "--loadeoprofile") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eoProfileOptions = Load;
+            options_.eoProfileName    = argv[i];
+        }
+        else if (arg == "--loadeeprofile") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eeProfileOptions = Load;
+            options_.eeProfileName    = argv[i];
+        }
+        else if (arg == "--loadofreepath") {
+
+            if (++i == argc)
+                return false;
+
+            options_.oFreePathOptions = Load;
+            options_.oFreePathName    = argv[i];
+        }
+        else if (arg == "--loadefreepath") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eFreePathOptions = Load;
+            options_.eFreePathName    = argv[i];
+        }
+        else if (arg == "--loadeeprobability") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eeProbabilityOptions = Load;
+            options_.eeProbabilityName    = argv[i];
+        }
+        else if (arg == "--loadeescfunction") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eEscFunctionOptions = Load;
+            options_.eEscFunctionName    = argv[i];
+        }
+        else if (arg == "--loadoescfunction") {
+
+            if (++i == argc)
+                return false;
+
+            options_.oEscFunctionOptions = Load;
+            options_.oEscFunctionName    = argv[i];
+        }
+        else if (arg == "--saveoeprofile") {
+
+            if (++i == argc)
+                return false;
+
+            options_.oeProfileOptions = Save;
+            options_.oeProfileName    = argv[i];
+        }
+        else if (arg == "--saveeoprofile") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eoProfileOptions = Save;
+            options_.eoProfileName    = argv[i];
+        }
+        else if (arg == "--saveeeprofile") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eeProfileOptions = Save;
+            options_.eeProfileName    = argv[i];
+        }
+        else if (arg == "--saveofreepath") {
+
+            if (++i == argc)
+                return false;
+
+            options_.oFreePathOptions = Save;
+            options_.oFreePathName    = argv[i];
+        }
+        else if (arg == "--saveefreepath") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eFreePathOptions = Save;
+            options_.eFreePathName    = argv[i];
+        }
+        else if (arg == "--saveeeprobability") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eeProbabilityOptions = Save;
+            options_.eeProbabilityName    = argv[i];
+        }
+        else if (arg == "--saveeescfunction") {
+
+            if (++i == argc)
+                return false;
+
+            options_.eEscFunctionOptions = Save;
+            options_.eEscFunctionName    = argv[i];
+        }
+        else if (arg == "--saveoescfunction") {
+
+            if (++i == argc)
+                return false;
+
+            options_.oEscFunctionOptions = Save;
+            options_.oEscFunctionName    = argv[i];
+        }
+
+        else
+            return false;
+    }
+
+    return true;
+}
+
+
+int ScatMCApp::run()
+{
+    using namespace std;
+
+    cout.precision(17);
+    cout << scientific;
+
+    cerr.precision(17);
+    cerr << scientific;
+
+    cerr << "# seed = "      << options_.seed       << endl;
+    cerr << "# photons = "   << options_.maxPhotons << endl;
+    cerr << "# H = "         << Optics::H           << endl;
+    cerr << "# lambda = "    << Optics::lambda      << endl;
+    cerr << "# thetaSize = " << options_.thetaSize  << endl;
+    cerr << "# phiSize = "   << options_.phiSize    << endl;
+    cerr << "# maxTheta = "  << options_.maxTheta   << endl;
+
+//FIXME    m_dataBuff.prepare();
+
+#if !defined TEST
+    //free path
+    if (!prepareFreePath<Optics::OBeam>(m_oLength, "o",
+                options_.oFreePathOptions, options_.oFreePathName)) {
+
+        return -1;
+    }
+#endif
+
+    if (!prepareFreePath<Optics::EBeam>(m_eLength, "e",
+                options_.eFreePathOptions, options_.eFreePathName)) {
+
+        return -1;
+    }
+
+    //e channel probability
+    if (!prepareEChannelProb(m_eChannelProb))
+        return -1;
+
+Partition pOE, pEO, pEE;
+
+#if !defined TEST
+    if (!preparePartition<IndicatrixOE>(pOE, "o-e",
+                options_.oeProfileOptions, options_.oeProfileName)) {
+
+        return -1;
+    }
+
+    if (!preparePartition<IndicatrixEO>(pEO, "e-o",
+                options_.eoProfileOptions, options_.eoProfileName)) {
+
+        return -1;
+    }
+#endif
+
+    if (!preparePartition<IndicatrixEE>(pEE, "e-e",
+                options_.eeProfileOptions, options_.eeProfileName)) {
+
+        return -1;
+    }
+
+ 
+#if !defined TEST
+    //free path
+    if (!prepareEscFunction<Optics::OBeam>(m_oEscFuncttion, "o",
+                options_.oEscFunctionOptions, options_.oEscFunctionName)) {
+
+        return -1;
+    }
+#endif
+
+    if (!prepareEscFunction<Optics::EBeam>(m_eEscFunction, "e",
+                options_.eEscFunctionOptions, options_.eEscFunctionName)) {
+
+        return -1;
+    }
+
+
+
+    cerr << "scattering..." << endl;
+    Photon::init(&m_oLength, &m_eLength, &pOE, &pEO, &pEE, &m_eChannelProb);
+
+    const int64_t flushRate = 1000;
+    m_saveRate  = omp_get_max_threads()*flushRate;
+
+    const Float t = 0.5*M_PI; //angle with director
+    //FIXME const Vector3 initVector = Vector3(cos(t), 0, sin(t)).normalize();
+    cerr << "initial angle: " << t << endl;
+
+
+    //main loop
+
+    #pragma omp parallel
+    {
+        RngEngine rng_engine;
+
+        rng_engine.seed(options_.seed + kSeedIncrement*omp_get_thread_num());
+
+        /*FIXME
+        int64_t scatteredCount = 0;
+        DataBuff buff(options_.points, options_.maxTime);
+
+
+        #pragma omp for schedule (dynamic)
+        for (int64_t i = 0; i < options_.maxPhotons; ++i) {
+
+            Photon ph(rng_engine, initVector, Optics::ECHANNEL);
+            size_t timeIdx = 0;
+
+            while ((ph.scatterings < options_.maxScatterings)
+                    && (ph.time <= options_.maxTime)) {
+
+                ph.move();
+
+                timeIdx = processScattering(ph, buff, timeIdx);
+
+                ph.scatter();
+
+            }
+
+            if (++scatteredCount == flushRate)
+                flushBuffers(scatteredCount, buff);
+        }
+
+        flushBuffers(scatteredCount, buff);
+        */
+    }
+
+    m_dataBuff.average();
+
+    output();
+    return 0;
+}
+
+
+void ScatMCApp::processScattering(const Photon& /*ph*/,
+                                  DataBuff& /*buff*/)
+{
+    //choose current indicatrix
+
+    //calculate 2d-array of contributions
+
+    //append to buff
+}
+
+void ScatMCApp::output()
+{
+    std::stringstream ss;
+    ss << options_.workDir << "out.txt";
+
+    std::fstream stream(ss.str().c_str()
+            , std::fstream::out | std::fstream::trunc);
+
+    stream << m_dataBuff;
+    stream.close();
+}
+
+void ScatMCApp::printHelp()
+{
+    using namespace std;
+
+    cerr 
+    << "Usage: " << options_.execName << "[options]" << endl
+
+    << "Available options:" << endl
+    << "--workdir [path]\t\t\toutput path" << endl
+    << "--seed [seed]\t\t\tseed for random numbers generator" << endl
+    << "--photons [photons]\t\t\tnumber of photons to scatter" << endl
+    << "--scatterings [scatterings]\t\t\tmax scatterings for each photon" << endl
+    << "--maxtime [maxtime]\t\t\tmax scattering time for each photon" << endl
+    << "--points [points]\t\t\tnumber of sampling points" << endl
+
+    << "--loadoeprofile [filename]\t\tload o-e profile from file"  << endl
+    << "--loadeoprofile [filename]\t\tload e-o profile from file"  << endl
+    << "--loadeeprofile [filename]\t\tload e-e profile from file"  << endl
+    << "--loadoefreepath [filename]\t\tload o-beam free path from file" << endl
+    << "--loadeofreepath [filename]\t\tload e-beam free path from file" << endl
+    << "--loadeeprobability [filename]\t\tload e-e probability from file" << endl
+
+    << "--saveoeprofile [filename]\t\tsave o-e profile to file"    << endl
+    << "--saveeoprofile [filename]\t\tsave e-o profile to file"    << endl
+    << "--saveeeprofile [filename]\t\tsave e-e profile to file" << endl
+    << "--saveoefreepath [filename]\t\tsave o-beam free path to file"  << endl
+    << "--saveeofreepath [filename]\t\tsave e-beam free path to file"  << endl
+    << "--saveeeprobability [filename]\t\tsave e-e probability to file"  << endl;
+}
+
+bool ScatMCApp::prepareEChannelProb(LinearInterpolation& l)
+{
+    using namespace std;
+
+    if (options_.eeProbabilityOptions == Load) {
+
+        cerr << "loading e-e probability file..." << endl;
+
+        if (!l.load(options_.eeProbabilityName)) {
+
+            cerr << "can't load e-e probability data" << endl;
+            return false;
+        }
+    }
+
+    if (   options_.eeProbabilityOptions == Create
+        || options_.eeProbabilityOptions == Save) {
+
+        cerr << "calculating e-e probability data..." << endl;
+        createEChannelProb<Optics::EBeam>(l);
+    }
+
+    if (options_.eeProbabilityOptions == Save) {
+
+        cerr << "saving e-e probability data to file..." << endl;
+
+        if (!l.save(options_.eeProbabilityName)) {
+
+            cerr << "can't save e-e probability data" << endl;
+            return false;
+        }
+    }
+
+    cerr << "done" << endl;
+
+    return true;
+}
+
+void ScatMCApp::flushBuffers(int64_t &scatteredCount, DataBuff &buff)
+{
+    #pragma omp critical
+    {
+        m_dataBuff += buff;
+        m_photonCnt += scatteredCount;
+
+        std::cerr << "Photons: " << m_photonCnt << std::endl;
+
+        if (0 == m_photonCnt % m_saveRate)
+            output();
+    }
+
+    scatteredCount = 0.;
+    buff.clear();
+}
---
+++ scatmc.h
@@ -0,0 +1,347 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "databuff.h"
+#include "interval.h"
+#include "distance.h"
+#include "partition.h"
+#include "escfunction.h"
+
+
+class Photon;
+
+
+class ScatMCApp
+{
+public:
+
+    ScatMCApp();
+
+    int  run();
+    bool getOpts(int argc, char ** argv);
+    void printHelp();
+
+    //random generator seeds in threads differ by this number
+    static const int   kSeedIncrement = 1000; 
+
+
+private:
+
+    enum FileOptions { Create, Load, Save };
+
+    struct Options
+    {
+        Options()
+          : execName()
+          , workDir()
+          , eeProfileOptions(Create)
+          , oeProfileOptions(Create)
+          , eoProfileOptions(Create)
+          , oFreePathOptions(Create)
+          , eFreePathOptions(Create)
+          , eeProbabilityOptions(Create)
+          , eEscFunctionOptions(Create)
+          , oEscFunctionOptions(Create)
+          , eeProfileName()
+          , oeProfileName()
+          , eoProfileName()
+          , oFreePathName()
+          , eFreePathName()
+          , eeProbabilityName()
+          , eEscFunctionName()
+          , oEscFunctionName()
+          , seed(1000)
+          , maxPhotons(1000)
+          , maxScatterings(1000)
+          , phiSize(100)
+          , thetaSize(100)
+          , escPhiSize(100)
+          , escThetaSize(100)
+          , escZSize(100)
+          , escMaxZ(1e-4)
+          , maxTheta(1e-4)
+        {}
+
+        std::string execName;
+        std::string workDir;
+
+        FileOptions eeProfileOptions;
+        FileOptions oeProfileOptions;
+        FileOptions eoProfileOptions;
+        FileOptions oFreePathOptions;
+        FileOptions eFreePathOptions;
+        FileOptions eeProbabilityOptions;
+        FileOptions eEscFunctionOptions;
+        FileOptions oEscFunctionOptions;
+        std::string eeProfileName;
+        std::string oeProfileName;
+        std::string eoProfileName;
+        std::string oFreePathName;
+        std::string eFreePathName;
+        std::string eeProbabilityName;
+        std::string eEscFunctionName;
+        std::string oEscFunctionName;
+
+        int     seed;
+        int64_t maxPhotons;
+        int64_t maxScatterings;
+        int     phiSize;
+        int     thetaSize;
+        int     escPhiSize;
+        int     escThetaSize;
+        int     escZSize;
+        Float   escMaxZ;
+        Float   maxTheta;
+    };
+
+    Options options_;
+
+    void flushBuffers(int64_t &scatteredCount, DataBuff &buff);
+    void output();
+    void processScattering(const Photon& ph, DataBuff& buff);
+
+
+    template <typename T>
+    bool  prepareFreePath(LinearInterpolation &l,
+                         const char *name,
+                         const int options,
+                         const std::string &fileName);
+
+    bool  prepareEChannelProb(LinearInterpolation& prob);
+
+    template <typename T>
+    bool  preparePartition(Partition &p,
+                           const char *name,
+                           const int options,
+                           const std::string &fileName);
+
+    template <typename T>
+    bool  prepareEscFunction(EscFunction &e,
+                         const char *name,
+                         const int options,
+                         const std::string &fileName);
+
+
+    LinearInterpolation m_eLength;
+    LinearInterpolation m_oLength;
+
+    LinearInterpolation m_eChannelProb;
+
+    EscFunction m_eEscFunction;
+    EscFunction m_oEscFunction;
+
+    int64_t m_photonCnt;
+    int64_t m_saveRate;
+
+    DataBuff m_dataBuff;
+
+private:
+
+    ScatMCApp& operator=(const ScatMCApp& other);
+    ScatMCApp(const ScatMCApp& other);
+};
+
+template <typename T>
+bool ScatMCApp::prepareFreePath(LinearInterpolation &l,
+                                const char *name,
+                                const int options,
+                                const std::string &fileName)
+{
+    using namespace std;
+
+    if (options == Load) {
+
+        cerr << "loading " << name << "-beam free path file..." << endl;
+
+        if (!l.load(fileName)) {
+
+            cerr << "can't load " << name << "-beam free path data" << endl;
+            return false;
+        }
+    }
+
+    if (options == Create || options == Save) {
+
+        cerr << "calculating " << name << "-beam free path data..." << endl;
+        createFreePath<T>(l);
+    }
+
+    if (options == Save) {
+
+        cerr << "saving " << name << "-beam free path data to file..." << endl;
+
+        if (!l.save(fileName)) {
+
+            cerr << "can't save " << name << "-beam free path data" << endl;
+            return false;
+        }
+    }
+
+    cerr << "done" << endl;
+
+    return true;
+}
+
+
+template <typename T>
+bool ScatMCApp::prepareEscFunction(EscFunction &e,
+                                   const char *name,
+                                   const int options,
+                                   const std::string &fileName)
+{
+    using namespace std;
+
+    if (options == Load) {
+
+        cerr << "loading " << name << "-beam escape function file..." << endl;
+
+        if (!e.load(fileName)) {
+
+            cerr << "can't load " << name << "-beam escape function" << endl;
+            return false;
+        }
+    }
+
+    if (options == Create || options == Save) {
+
+        cerr << "calculating " << name << "-beam escape function data..." << endl;
+        e.create<T>(m_oLength, m_eLength,
+                options_.escPhiSize, options_.escThetaSize,
+                options_.escZSize, options_.escMaxZ);
+    }
+
+    if (options == Save) {
+
+        cerr << "saving " << name << "-beam escape function to file..." << endl;
+
+        if (!e.save(fileName)) {
+
+            cerr << "can't save " << name << "-beam escape function" << endl;
+            return false;
+        }
+    }
+
+    cerr << "done" << endl;
+
+    return true;
+}
+
+
+template <typename T>
+bool ScatMCApp::preparePartition(Partition &p,
+                                 const char *name,
+                                 const int options,
+                                 const std::string &fileName)
+{
+    using namespace std;
+
+    if (options == Load) {
+
+        cerr << "loading " << name << " profile file..." << endl;
+
+        if (!p.load(fileName)) {
+
+            cerr << "can't load " << name << " profile data" << endl;
+            return false;
+        }
+    }
+
+    if (options == Create || options == Save) {
+
+        typedef std::vector<Interval>  IntervalsContainer;
+        IntervalsContainer intervals;
+
+
+        cerr << "calculating profile intervals..." << endl;
+
+        Float left = 0.01;
+        Float right = 0.5* M_PI;
+
+        //const Float tolerance = 0.1;
+        const Float tolerance = 0.01;
+        //const Float tolerance = 0.001;
+
+        Float dist;
+        int i = 0;
+
+        intervals.push_back(Interval(0., left));
+
+#if !defined TEST
+        int num_threads = omp_get_max_threads();
+#else
+        int num_threads = 1;
+#endif
+
+        #pragma omp parallel for
+        for (int j = 0; j < num_threads; ++j) {
+
+            Float local_left = left + j/(Float)num_threads*(right-left);
+            Float local_right = left + (j+1)/(Float)num_threads*(right-left);
+            Float local_end = local_right;
+
+            do {
+
+                local_right = local_end;
+
+                T ind_left = createIndicatrix<T>(local_left);
+
+                while (1) {
+
+                    T ind_right  = createIndicatrix<T>(local_right);
+
+                    dist = ::distance(ind_left, ind_right, 0.01*tolerance);
+
+                    if (dist > tolerance)
+                        local_right = 0.5*(local_right + local_left);
+                    else
+                        break;
+
+                                    }
+
+                #pragma omp critical
+                {
+                    intervals.push_back(Interval(local_left, local_right));
+                    std::cout << i << " "
+                              << local_left << " "
+                              << local_right << std::endl;
+
+                    i++;
+                }
+
+                local_left = local_right;
+
+            }
+            while (local_right != local_end);
+        }
+
+        std::sort(intervals.begin(), intervals.end());
+
+        cerr << "calculating " << name << " profile data..." << endl;
+
+        const int chunksNum = intervals.size();
+//#if !defined TEST
+        #pragma omp parallel for schedule(dynamic)
+//#endif
+        for (int i = 0; i < chunksNum; ++i) {
+
+              p.addChunk<T>(intervals[i].left, intervals[i].right);
+        }
+    }
+
+    if (options == Save) {
+
+        cerr << "saving " << name << " profile data to file..." << endl;
+
+        if (!p.save(fileName)) {
+
+            cerr << "can't save " << name << " profile data" << endl;
+            return false;
+        }
+    }
+
+    cerr << "done" << endl;
+
+    return true;
+}
